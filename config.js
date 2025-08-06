7b1e0dd936f58ff3a95a9e9374599f8f0ec829d3
lasithdulshan637-hub <lasithdulshan637@gmail.com>
2025-08-06 18:40:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
+SESSION_ID: process.env.SESSION_ID || "qMV0wKCD#rpqx7KbjLrD5aI_wWwMBlx7jnTb_8zk0ZAM587iD9m0", // ඔයාගෙ session id එක
 MONGODB: process.env.MONGODB || "mongodb://mongo:BtlElddMPOAvDVdGaFrCxqPFFNJAmyMg@yamabiko.proxy.rlwy.net:57963",  //ඔයාගෙ mongoDb url එක
 };
  
